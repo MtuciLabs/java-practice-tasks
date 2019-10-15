f972a8ea560249af8d8730a7e76bd47249663683
Iriha <irina.schorina@gmail.com>
2019-10-16 00:01:30 +0300

    part01_Task21

--- src/main/java/lesson02/part01/Task21.java
+++ src/main/java/lesson02/part01/Task21.java
@@ -15,6 +15,8 @@ package lesson02.part01;
 
 public class Task21 {
     public static void main(String[] args) {
+        for (int i=1; i<=10; i++)
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
         //напишите тут ваш код
     }
 }
