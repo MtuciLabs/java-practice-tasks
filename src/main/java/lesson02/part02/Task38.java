d083c6dface3779791684e68fa0f22c5f038030a
Iriha <irina.schorina@gmail.com>
2019-10-25 23:18:23 +0300

    part02_Task38

--- src/main/java/lesson02/part02/Task38.java
+++ src/main/java/lesson02/part02/Task38.java
@@ -16,6 +16,14 @@ package lesson02.part02;
 
 public class Task38 {
     public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.print(8);
+        }
+        System.out.println();
+        System.out.println("-----");
+        for (int j=1; j<=10; j++){
+            System.out.println(8);
+        }
         //напишите тут ваш код
 
     }
