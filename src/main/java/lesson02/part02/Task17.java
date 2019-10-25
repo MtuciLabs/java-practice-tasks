e9e70cfe13053fe02e5a8357cfea221b7477a18b
Iriha <irina.schorina@gmail.com>
2019-10-25 22:52:11 +0300

    part02_Task17

--- src/main/java/lesson02/part02/Task17.java
+++ src/main/java/lesson02/part02/Task17.java
@@ -1,5 +1,7 @@
 package lesson02.part02;
 
+import java.util.Scanner;
+
 /**
  * Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
  * Если такая пара существует, вывести на экран числа через пробел. Если все три числа равны между собой, то вывести все три.
@@ -29,6 +31,22 @@ package lesson02.part02;
 
 public class Task17 {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        int b = scanner.nextInt();
+        System.out.println("Введите третье число:");
+        int c = scanner.nextInt();
+        if ((a==b)& (b==c) &(a==c))
+            System.out.println(a+" "+b+" "+c);
+        else if (a==b)
+            System.out.println(a +" "+b);
+        else if (a==c)
+            System.out.println(a+" "+c);
+        else if (b==c)
+            System.out.println(b+" "+c);
+
         //напишите тут ваш код
 
     }
