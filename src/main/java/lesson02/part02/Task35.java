0b84ef82c82fdcf670a3d2c0f4b8d320996b3bd1
Iriha <irina.schorina@gmail.com>
2019-10-25 23:17:41 +0300

    part02_Task35

--- src/main/java/lesson02/part02/Task35.java
+++ src/main/java/lesson02/part02/Task35.java
@@ -14,6 +14,10 @@ package lesson02.part02;
 
 public class Task35 {
     public static void main(String[] args) {
+        for (int i=1; i<=100; i++) {
+            if (i%2==0)
+                System.out.println(i);
+        }
         //напишите тут ваш код
 
     }
