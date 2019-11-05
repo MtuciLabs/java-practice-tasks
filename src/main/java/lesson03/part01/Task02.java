faac019aee240ac67684cfa7745912cfe532301e
Iriha <irina.schorina@gmail.com>
2019-11-05 18:03:26 +0300

    part01.Task02

--- src/main/java/lesson03/part01/Task02.java
+++ src/main/java/lesson03/part01/Task02.java
@@ -21,6 +21,21 @@ public class Task02 {
     }
 
     public static class Dog {
+        String name;
+        int age;
+
+        public String getName() {
+            return name;
+        }
+        public  void setName (String name) {
+            this.name=name;
+        }
+        public int getAge (){
+            return age;
+        }
+        public void setAge(int age){
+            this.age=age;
+        }
         //напишите тут ваш код
 
     }
