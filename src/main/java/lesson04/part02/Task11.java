798f72239b75255442d0145fd3e729a587c1ade0
Mansy <shaidullinmansur@gmail.com>
2019-11-17 18:08:05 +0300

    lesson04

--- src/main/java/lesson04/part02/Task11.java
+++ src/main/java/lesson04/part02/Task11.java
@@ -3,15 +3,16 @@ package lesson04.part02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Ввести с клавиатуры 2 числа N и M.
  * Ввести N строк и заполнить ими список.
  * Переставить M первых строк в конец списка.
  * Вывести список на экран, каждое значение с новой строки.
- *
+ * <p>
  * Примечание: запрещено создавать больше одного списка.
- *
+ * <p>
  * Требования:
  * 1.	Объяви переменную типа список строк и сразу проинициализируй ee.
  * 2.	Считай c клавиатуры числа N и M, считай N строк и добавь их в список.
@@ -20,9 +21,21 @@ import java.io.InputStreamReader;
  */
 
 public class Task11 {
-  public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-    //напишите тут ваш код
-  }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        System.out.println("");
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            list.add(list.get(0));
+            list.remove(0);
+        }
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
 }
