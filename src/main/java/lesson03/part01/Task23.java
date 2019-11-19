dbd7e5992f1b8eaf43e997615502d4ed4ac0811a
Iriha <irina.schorina@gmail.com>
2019-11-19 18:15:08 +0300

    part01_Task23

--- src/main/java/lesson03/part01/Task23.java
+++ src/main/java/lesson03/part01/Task23.java
@@ -17,13 +17,28 @@ package lesson03.part01;
 public class Task23 {
 
   public static void main(String[] args) {
+    Cat cat1 = new Cat();
+    Cat cat2 = new Cat();
+    Cat cat3 = new Cat();
+    Cat cat4 = new Cat();
+    Cat cat5 = new Cat();
+    Cat cat6 = new Cat();
+    Cat cat7 = new Cat();
+    Cat cat8 = new Cat();
+    Cat cat0 = new Cat();
+    Cat cat10 = new Cat();
+    System.out.println(Cat.catCount);
     // Создай 10 котов
 
     // Выведи значение переменной catCount
   }
 
   public static class Cat {
+    public static int catCount = 0;
     // Создай статическую переменную catCount
+    public Cat(){
+      catCount++;
+    }
 
     // Создай конструктор
   }
