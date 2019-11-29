229b99237bd4536ff61313f320c95ecd5d164746
belyashik169 <maska8.8.8@yandex.ru>
2019-11-29 13:06:46 +0300

    Сделала задания lesson 2 part 1.

--- src/main/java/lesson02/part01/Task03.java
+++ src/main/java/lesson02/part01/Task03.java
@@ -17,12 +17,14 @@ package lesson02.part01;
 
 public class Task03 {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        System.out.println(convertEurToUsd(123,435));
+        System.out.println(convertEurToUsd(88,55));
 
     }
 
     public static double convertEurToUsd(int eur, double course) {
-        //напишите тут ваш код
-        return 0d;
+        double dollar;
+        dollar=eur*course;
+        return dollar;
     }
 }
