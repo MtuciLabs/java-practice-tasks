b2413ee9365d74eeeeb0d92763241589311f896a
Denis Li <den.lugovenko@mail.ru>
2019-12-22 00:19:30 +0300

    Update Task01.java

--- src/main/java/lesson01/part1/Task01.java
+++ src/main/java/lesson01/part1/Task01.java
@@ -1,3 +1,4 @@
+    
 package lesson01.part1;
 
 public class Task01 {
@@ -15,6 +16,7 @@ public class Task01 {
      */
 
     public static void main(String[] args) {
+        System.out.println("Привет мир!");
         // Напишите здесь ваш код
     }
 }
