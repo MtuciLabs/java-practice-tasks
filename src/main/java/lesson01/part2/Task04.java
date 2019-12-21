d17ffd4ddf7c25db764a12059bb7e122da8d2d2d
Denis Li <den.lugovenko@mail.ru>
2019-12-22 01:06:21 +0300

    Update Task04.java

--- src/main/java/lesson01/part2/Task04.java
+++ src/main/java/lesson01/part2/Task04.java
@@ -23,6 +23,10 @@ public class Task04 {
      */
 
     public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a*b);
         //напишите тут ваш код
     }
 
