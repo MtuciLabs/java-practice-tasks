56b11b9b3c11e01b5e7e41973db05c306aa7da30
Denis Li <den.lugovenko@mail.ru>
2019-12-22 17:34:22 +0300

    Update Task04.java

--- src/main/java/lesson02/part02/Task04.java
+++ src/main/java/lesson02/part02/Task04.java
@@ -14,18 +14,23 @@ package lesson02.part02;
 
 public class Task04 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    Cat cat = new Cat();
+    cat.addNewCat();
+    System.out.println(Cat.catsCount);
+
+
+  }
+
+  public static class Cat {
+
+    private static int catsCount = 0;
+
+    public static void addNewCat() {
+      ++Cat.catsCount;
+      //напишите тут ваш код
 
     }
-
-    public static class Cat {
-        private static int catsCount = 0;
-
-        public static void addNewCat() {
-            //напишите тут ваш код
-
-        }
-    }
+  }
 
 }
-
