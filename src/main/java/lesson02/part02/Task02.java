6f86a44e6a97d60249a2aaedd6e8651ac2bb989f
Denis Li <den.lugovenko@mail.ru>
2019-12-22 17:33:19 +0300

    Update Task02.java

--- src/main/java/lesson02/part02/Task02.java
+++ src/main/java/lesson02/part02/Task02.java
@@ -14,20 +14,23 @@ package lesson02.part02;
  */
 
 public class Task02 {
-    public static void main(String[] args) {
-        Apple apple = new Apple();
-        apple.addPrice(50);
-        Apple apple2 = new Apple();
-        apple2.addPrice(100);
-        System.out.println("Стоимость яблок " + Apple.applesPrice);
+
+  public static void main(String[] args) {
+    Apple apple = new Apple();
+    apple.addPrice(50);
+    Apple apple2 = new Apple();
+    apple2.addPrice(100);
+    System.out.println("Стоимость яблок " + Apple.applesPrice);
+  }
+
+  public static class Apple {
+
+    public static int applesPrice = 0;
+
+    public static void addPrice(int applesPrice) {
+      Apple.applesPrice = Apple.applesPrice + applesPrice;
+      //напишите тут ваш код
+
     }
-
-    public static class Apple {
-        public static int applesPrice = 0;
-
-        public static void addPrice(int applesPrice) {
-            //напишите тут ваш код
-
-        }
-    }
+  }
 }
