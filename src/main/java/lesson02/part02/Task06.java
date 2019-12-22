93e10d042f7af18376243ee5e7d38f07eae31dd7
Egor <polzikov0510@gmail.com>
2019-12-22 15:51:26 +0300

    lesson2.

--- src/main/java/lesson02/part02/Task06.java
+++ src/main/java/lesson02/part02/Task06.java
@@ -1,30 +1,34 @@
 package lesson02.part02;
 
 /**
- * Допишите код метода setName, чтобы с его помощью установить значение private String fullName равное значению локальной переменной String fullName.
+ * Допишите код метода setName, чтобы с его помощью установить значение private String fullName равное значению
+ * локальной переменной String fullName.
  * <p>
  * <p>
- * Требования:
- * 1.	Класс Cat должен содержать только одну переменную fullName.
- * 2.	Переменная fullName должна иметь модификатор доступа private и тип String.
- * 3.	Класс Cat должен содержать только два метода setName и main.
- * 4.	Метод setName должен иметь локальную переменную fullName.
- * 5.	Метод setName класса Cat должен устанавливать значение глобальной переменной private String fullName равным локальной переменной fullName.
+ * Требования: 1.	Класс Cat должен содержать только одну переменную fullName. 2.	Переменная fullName должна иметь
+ * модификатор доступа private и тип String. 3.	Класс Cat должен содержать только два метода setName и main. 4.	Метод
+ * setName должен иметь локальную переменную fullName. 5.	Метод setName класса Cat должен устанавливать значение
+ * глобальной переменной private String fullName равным локальной переменной fullName.
  */
 
 public class Task06 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    Cat cat = new Cat();
+    cat.setName("Васька", "Барсик");
+    System.out.println(cat.fullName);
 
+  }
+
+  public static class Cat {
+
+    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+      String fullName = firstName + " " + lastName;
+      this.fullName = fullName;
+
+      //напишите тут ваш код
     }
-
-    public static class Cat {
-        private String fullName;
-
-        public void setName(String firstName, String lastName) {
-            String fullName = firstName + " " + lastName;
-
-            //напишите тут ваш код
-        }
-    }
+  }
 }
