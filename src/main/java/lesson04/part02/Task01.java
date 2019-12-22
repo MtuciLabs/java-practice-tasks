49a8adab6784977ee1786bea51db5f1fe4a0a927
Egor <polzikov0510@gmail.com>
2019-12-22 16:00:42 +0300

    Lesson4

---
+++ src/main/java/lesson04/part02/Task01.java
@@ -0,0 +1,33 @@
+package lesson04.part02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 1. Создать массив на 10 строк.
+ * 2. Ввести с клавиатуры 8 строк и сохранить их в массив.
+ * 3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.
+ * Каждый элемент - с новой строки.
+ * <p>
+ * Требования:
+ * 1.	Программа должна создавать массив на 10 строк.
+ * 2.	Программа должна считывать 8 строк для массива с клавиатуры.
+ * 3.	Программа должна выводить на экран 10 строк, каждую с новой строки.
+ * 4.	Программа должна выводить на экран массив (10 элементов) в обратном порядке.
+ */
+
+public class Task01 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = new String[10];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = r.readLine();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ src/main/java/lesson04/part02/Task04.java
@@ -0,0 +1,43 @@
+package lesson04.part02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * 1. Создай список строк.
+ * 2. Считай с клавиатуры 5 строк и добавь в список.
+ * 3. Используя цикл, найди самую длинную строку в списке.
+ * 4. Выведи найденную строку на экран. Если таких строк несколько, выведи каждую с новой строки.
+ * <p>
+ * Требования:
+ * 1.	В методе main инициализируй существующее поле strings новым ArrayList<>
+ * 2.	Программа должна считывать 5 строк с клавиатуры и записывать их в поле strings.
+ * 3.	Программа должна выводить самую длинную строку на экран.
+ * 4.	Если есть несколько строк с длиной равной максимальной, то нужно вывести каждую из них
+ * с новой строки.
+ */
+
+public class Task04 {
+
+    private static ArrayList<String> strings;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String s = r.readLine();
+            strings.add(s);
+        }
+        int max = strings.get(0).length();
+        for (int i = 0; i < 5; i++) {
+            if (strings.get(i).length() >= max) {
+                max = strings.get(i).length();
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (strings.get(i).length() == max)
+                System.out.println(strings.get(i));
+        }
+    }
+}
