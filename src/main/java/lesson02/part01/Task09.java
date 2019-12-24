c037800b5504ed91d40a5d8ea421ca2a1d0e648a
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 14:16:51 +0300

    Update Task09.java

--- src/main/java/lesson02/part01/Task09.java
+++ src/main/java/lesson02/part01/Task09.java
@@ -23,6 +23,11 @@ package lesson02.part01;
 public class Task09 {
     public static void main(String[] args) {
         //напишите тут ваш код
+        int s=0;
+        for (int i=1; i<6;i++){
+            s=s+i;
+            System.out.println(s);
+        }
 
     }
 }
