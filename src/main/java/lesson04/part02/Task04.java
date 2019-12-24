6704ae65069951d2b38f17fbbd4089f73fff5a23
DanilPodpalyi <danil.podpalyi@gmail.com>
2019-12-24 18:06:07 +0300

    commit 1-5

--- src/main/java/lesson04/part02/Task04.java
+++ src/main/java/lesson04/part02/Task04.java
@@ -3,19 +3,23 @@ package lesson04.part02;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 1. Создай список строк.
  * 2. Считай с клавиатуры 5 строк и добавь в список.
  * 3. Используя цикл, найди самую длинную строку в списке.
  * 4. Выведи найденную строку на экран. Если таких строк несколько, выведи каждую с новой строки.
- * <p>
+ *
  * Требования:
  * 1.	В методе main инициализируй существующее поле strings новым ArrayList<>
  * 2.	Программа должна считывать 5 строк с клавиатуры и записывать их в поле strings.
  * 3.	Программа должна выводить самую длинную строку на экран.
  * 4.	Если есть несколько строк с длиной равной максимальной, то нужно вывести каждую из них
  * с новой строки.
+ * if (max==st.length()){
+ *         System.out.println(st);
+ *       }
  */
 
 public class Task04 {
@@ -23,21 +27,26 @@ public class Task04 {
     private static ArrayList<String> strings;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            String s = r.readLine();
-            strings.add(s);
+        //напишите тут ваш код
+        ArrayList<String> strings= new ArrayList<String>();
+        Scanner scn= new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            strings.add(scn.next());
         }
-        int max = strings.get(0).length();
-        for (int i = 0; i < 5; i++) {
-            if (strings.get(i).length() >= max) {
-                max = strings.get(i).length();
+        int max=0;
+        String maxS = null;
+        for(int i=0;i<5;i++) {
+            String st = strings.get(i);
+            if (max < st.length()) {
+                max = st.length();
+                maxS = st;
             }
         }
-        for (int i = 0; i < 5; i++) {
-            if (strings.get(i).length() == max)
-                System.out.println(strings.get(i));
+        for (int i=0;i<5;i++){
+            String st=strings.get(i);
+            if (max==st.length()){
+                System.out.println(st);
+            }
         }
     }
 }
