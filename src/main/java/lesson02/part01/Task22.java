888ae848800a251fc574f0ba5f0e312e852ade85
Artyom333 <artyom458787@gmail.com>
2019-12-24 21:16:49 +0300

    ошибки lesson 2

--- src/main/java/lesson02/part01/Task22.java
+++ src/main/java/lesson02/part01/Task22.java
@@ -1,7 +1,9 @@
 package lesson02.part01;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Ввести с клавиатуры число n.
