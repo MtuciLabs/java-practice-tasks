a00fc76c0a2ad009e8cac26451a4e3bae1df5737
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 22:19:05 +0300

    Update Task09.java

--- src/main/java/lesson05/part01/Task09.java
+++ src/main/java/lesson05/part01/Task09.java
@@ -14,21 +14,11 @@ package lesson05.part01;
  * 5. Методы, с помощью которых классы Cat и Dog взаимодействуют друг с другом, должны быть публичными.
  */
 
-public class Task09 {
-    public static void main(String[] args) {
-        Cat cat = new Cat("Vaska", 5);
-        Dog dog = new Dog("Sharik", 4);
-
-        cat.isDogNear(dog);
-        dog.isCatNear(cat);
-    }
-}
-
 class Cat {
     private String name;
     private int speed;
 
-    Cat(String name, int speed) {
+    public Cat(String name, int speed) {
         this.name = name;
         this.speed = speed;
     }
@@ -37,7 +27,7 @@ class Cat {
         return name;
     }
 
-    int getSpeed() {
+    public int getSpeed() {
         return speed;
     }
 
@@ -50,7 +40,7 @@ class Dog {
     private String name;
     private int speed;
 
-    Dog(String name, int speed) {
+    public Dog(String name, int speed) {
         this.name = name;
         this.speed = speed;
     }
@@ -59,7 +49,7 @@ class Dog {
         return name;
     }
 
-    int getSpeed() {
+    public int getSpeed() {
         return speed;
     }
 
@@ -67,4 +57,3 @@ class Dog {
         return this.speed > cat.getSpeed();
     }
 }
-
