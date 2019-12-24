edd2a0331c69235b3506de0dc9e07e5f16bf3dd3
dimamgn74@gmail.com <dima5006819>
2019-12-24 13:55:40 +0300

    lesson01 1-5

--- src/main/java/lesson01/part1/Task01.java
+++ src/main/java/lesson01/part1/Task01.java
@@ -15,7 +15,7 @@ public class Task01 {
      */
 
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        System.out.println("Hello World!");
         // Напишите здесь ваш код
     }
 }
