7455e0a86bc5d210bf6f38a3fdb2dfa181db9cd8
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 19:35:11 +0300

    Update Task20.java

--- src/main/java/lesson04/part02/Task20.java
+++ src/main/java/lesson04/part02/Task20.java
@@ -20,39 +20,43 @@ import java.util.List;
 
 public class Task20 {
 
-  public static void main(String[] args) {
-    // ArrayList
-    ArrayList arrayList = new ArrayList();
-    insert10000(arrayList);
-    get10000(arrayList);
-    set10000(arrayList);
-    remove10000(arrayList);
+    public static void main(String[] args) {
+        // ArrayList
+        ArrayList arrayList = new ArrayList();
+        insert10000(arrayList);
+        get10000(arrayList);
+        set10000(arrayList);
+        remove10000(arrayList);
 
-    // LinkedList
-    LinkedList linkedList = new LinkedList();
-    insert10000(linkedList);
-    get10000(linkedList);
-    set10000(linkedList);
-    remove10000(linkedList);
-  }
+        // LinkedList
+        LinkedList linkedList = new LinkedList();
+        insert10000(linkedList);
+        get10000(linkedList);
+        set10000(linkedList);
+        remove10000(linkedList);
+    }
 
-  public static void insert10000(List list) {
-    //напишите тут ваш код
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(i, i);
+        }
+    }
 
-  }
+    public static void get10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.get(i);
+        }
+    }
 
-  public static void get10000(List list) {
-    //напишите тут ваш код
+    public static void set10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.set(i,list.get(i));
+        }
+    }
 
-  }
-
-  public static void set10000(List list) {
-    //напишите тут ваш код
-
-  }
-
-  public static void remove10000(List list) {
-    //напишите тут ваш код
-
-  }
+    public static void remove10000(List list) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            list.remove(i);
+        }
+    }
 }
