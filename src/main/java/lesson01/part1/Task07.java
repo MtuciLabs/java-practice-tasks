98223dc3efcd764dee82de8ba66848789824358b
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 13:41:26 +0300

    Update Task07.java

--- src/main/java/lesson01/part1/Task07.java
+++ src/main/java/lesson01/part1/Task07.java
@@ -26,8 +26,8 @@ public class Task07 {
     }
 
     public static double convertCelsiusToFahrenheit(int celsius) {
-        //напишите тут ваш код
+        double TF = (9.0/5.0) * celsius+32; //напишите тут ваш код
 
-        return 0;
+        return TF;
     }
 }
