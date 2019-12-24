3163fb2bd96bfbf29962a46155c30843a1f6a25c
DanilPodpalyi <danil.podpalyi@gmail.com>
2019-12-24 17:55:32 +0300

    commit 1-5

--- src/main/java/lesson02/part02/Task13.java
+++ src/main/java/lesson02/part02/Task13.java
@@ -16,35 +16,33 @@ import java.util.Scanner;
 public class Task13 {
 
   public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Введите день ");
-    int day = sc.nextInt();
-
-    switch (day) {
-      case 1:
-        System.out.println("Monday");
-        break;
-      case 2:
-        System.out.println("Tuesday");
-        break;
-      case 3:
-        System.out.println("Wednesday");
-        break;
-      case 4:
-        System.out.println("Thursday");
-        break;
-      case 5:
-        System.out.println("Friday");
-        break;
-      case 6:
-        System.out.println("Saturday");
-        break;
-      case 7:
-        System.out.println("Sunday");
-        break;
-      default:
-        System.out.println("Такого дня недели не существует");
+    Scanner scn = new Scanner(System.in);
+    int day = scn.nextInt();
+    if(day==1){
+      System.out.println("Понедельник");
     }
+    if(day==2){
+      System.out.println("Вторник");
+    }
+    if(day==3){
+      System.out.println("Среда");
+    }
+    if(day==4){
+      System.out.println("Четверг");
+    }
+    if(day==5){
+      System.out.println("Пятница");
+    }
+    if(day==6){
+      System.out.println("Суббота");
+    }
+    if(day==7){
+      System.out.println("Воскресенье");
+    }
+    if (day>7 || day<1){
+      System.out.println("такого дня недели не существует");
+    }
+
 
   }
 }
