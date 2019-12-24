375cd3c869811e9066c7567333b7f9f7c4038f33
Denis Li <den.lugovenko@mail.ru>
2019-12-25 00:26:23 +0300

    Update Task19.java

--- src/main/java/lesson02/part01/Task19.java
+++ src/main/java/lesson02/part01/Task19.java
@@ -12,7 +12,8 @@ package lesson02.part01;
  * 3.	Выведенный текст должен содержать введенное имя.
  * 4.	Выведенный текст должен полностью соответствовать заданию.
  */
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Task19 {
     public static void main(String[] args) throws Exception {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
