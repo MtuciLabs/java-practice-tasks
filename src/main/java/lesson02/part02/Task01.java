9012472ae3dce92aaf1a765d8317ae3ba48d905e
DanilPodpalyi <danil.podpalyi@gmail.com>
2019-12-24 17:54:35 +0300

    commit 1-5

--- src/main/java/lesson02/part02/Task01.java
+++ src/main/java/lesson02/part02/Task01.java
@@ -12,21 +12,18 @@ package lesson02.part02;
  */
 
 public class Task01 {
-
   public static void main(String[] args) {
     Person person = new Person();
     System.out.println("Age is: " + person.age);
     person.adjustAge(person.age);
     System.out.println("Adjusted age is: " + person.age);
   }
-
   public static class Person {
-
     public int age = 20;
 
     public void adjustAge(int age) {
-      this.age = age + 20;
-      System.out.println("Age in adjustAge is: " + age);
+      this.age= age + 20;
+      System.out.println("Age in adjustAge() is: " + age);
     }
   }
 }
