735ee1fd83bf02e2220377fe0b40b146ab381f9c
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 14:24:34 +0300

    Update Task17.java

--- src/main/java/lesson02/part01/Task17.java
+++ src/main/java/lesson02/part01/Task17.java
@@ -1,31 +1,33 @@
 package lesson02.part01;
 
 /**
- * Ввести с клавиатуры число и имя, вывести на экран строку:
- * «имя» захватит мир через «число» лет. Му-ха-ха!
- * Пример: Вася захватит мир через 8 лет. Му-ха-ха!
- * <p>
- * Последовательность вводимых данных имеет большое значение.
+ * Ввести с клавиатуры отдельно Имя, число1, число2. Вывести надпись:
+ * «Имя» получает «число1» через «число2» лет.
+ * Пример: Коля получает 3000 через 5 лет.
  * <p>
  * <p>
  * Требования:
  * 1.	Программа должна выводить текст.
  * 2.	Программа должна считывать данные с клавиатуры.
  * 3.	Выведенный текст должен содержать введенное имя.
- * 4.	Выведенный текст должен содержать введенное число.
- * 5.	Выведенный текст должен полностью соответствовать заданию.
+ * 4.	Выведенный текст должен содержать введенное число1.
+ * 5.	Выведенный текст должен содержать введенное число2.
+ * 6.	Выведенный тест должен полностью соответствовать заданию.
  */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Task17 {
+public class Task18 {
     public static void main(String[] args) throws Exception {
-      BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-      System.out.println("Введите имя: ");
-      String name = consoleReader.readLine();
-      System.out.println("Введите число: ");
-      String age = consoleReader.readLine();
-      System.out.println(name + " захватит мир через " + age + " лет. Му-ха-ха!");
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите имя: ");
+        String name = consoleReader.readLine();
+        System.out.println("Введите число1: ");
+        String num1 = consoleReader.readLine();
+        System.out.println("Введите число2: ");
+        String num2 = consoleReader.readLine();
+        System.out.println(name + " получает " + num1 + " через " + num2 + " лет.");
+
     }
 }
