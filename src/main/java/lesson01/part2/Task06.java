396ffd5e20ae53a31c964dc1795fe9177f51c76d
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 13:57:41 +0300

    Update Task06.java

--- src/main/java/lesson01/part2/Task06.java
+++ src/main/java/lesson01/part2/Task06.java
@@ -16,6 +16,7 @@ public class Task06 {
      */
 
     public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
         //напишите тут ваш код
     }
 }
