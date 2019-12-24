64b70ef5079bd835697dc56c1e014eda1ea257a3
DrcConan <43205042+DrcConan@users.noreply.github.com>
2019-12-24 16:49:36 +0300

    Update Task05.java

--- src/main/java/lesson04/part01/Task05.java
+++ src/main/java/lesson04/part01/Task05.java
@@ -3,7 +3,7 @@ package lesson04.part01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.ArrayList;
 /**
  * Ввести с клавиатуры 10 чисел и заполнить ими список.
  * Вывести их в обратном порядке. Каждый элемент - с новой строки.
@@ -20,9 +20,14 @@ import java.io.InputStreamReader;
  */
 
 public class Task05 {
-  public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-    //напишите тут ваш код
-  }
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(r.readLine()));
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.println(list.get(i));
+        }
+    }
 }
