5e93ec29576778b681a51a2579eaef22fe6aceac
Максим <dragovx@mail.ru>
2019-12-24 13:21:00 +0300

    Lessons 1(1,2)

--- src/main/java/lesson01/part1/Task01.java
+++ src/main/java/lesson01/part1/Task01.java
@@ -15,7 +15,7 @@ public class Task01 {
      */
 
     public static void main(String[] args) {
-        System.out.println("Привет мир!");
+        System.out.println("Hello world!");
         // Напишите здесь ваш код
     }
 }
