8642a7bc8548a2d2718a430cf84a3e8bed92c52e
Denis Li <den.lugovenko@mail.ru>
2019-12-24 23:30:31 +0300

    Update Task01.java

--- src/main/java/lesson02/part02/Task01.java
+++ src/main/java/lesson02/part02/Task01.java
@@ -16,7 +16,6 @@ package lesson02.part02;
  */
 
 public class Task01 {
-
   public static void main(String[] args) {
     Person person = new Person();
     System.out.println("Age is: " + person.age);
@@ -25,12 +24,11 @@ public class Task01 {
   }
 
   public static class Person {
-
     public int age = 20;
 
     public void adjustAge(int age) {
-      this.age = age + 20;
-      System.out.println("Age in adjustAge is: " + age);
+      this.age= age + 20;
+      System.out.println("Age in adjustAge() is: " + age);
     }
   }
 }
